82757c4295ab6ea8d488c7158310cf816faeac9a
BresoDEV <75597388+BresoDEV@users.noreply.github.com>
2020-12-20 23:40:04 -0300

    Add files via upload

---
+++ RDR2/cadastro.php
@@ -0,0 +1,40 @@
+ <?php
+
+$login = $_POST['login'];
+$senha = MD5($_POST['senha']);
+$connect = mysql_connect('localhost','id14124763_wp_e71c32ec1d7de1978bee8cd16e05d98c','wB{G~+e#8n%6#8Qm');
+$db = mysql_select_db('id14124763_wp_e71c32ec1d7de1978bee8cd16e05d98c');
+$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
+$select = mysql_query($query_select,$connect);
+$array = mysql_fetch_array($select);
+$logarray = $array['login'];
+
+  if($login == "" || $login == null){
+    echo"<script language='javascript' type='text/javascript'>
+    alert('O campo login deve ser preenchido');window.location.href='
+    cadastro.html';</script>";
+
+    }else{
+      if($logarray == $login){
+
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Esse login já existe');window.location.href='
+        cadastro.html';</script>";
+        die();
+
+      }else{
+        $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
+        $insert = mysql_query($query,$connect);
+
+        if($insert){
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Usuário cadastrado com sucesso!');window.location.
+          href='login.html'</script>";
+        }else{
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Não foi possível cadastrar esse usuário');window.location
+          .href='cadastro.html'</script>";
+        }
+      }
+    }
+?>
---
+++ RDR2/index.php
@@ -0,0 +1,12 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Meu Redirect</title>
+
+    <meta http-equiv="refresh" content="1; URL='https://servidorbreso.000webhostapp.com/RDR2Menu/pages/examples/sign-in.html'"/>
+</head>
+<body>
+...
+</body>
+</html>
